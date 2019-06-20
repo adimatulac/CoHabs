2b0999a6ad715eee67dc2f0e179d29eb20dd58b0
Jason <jsykim33@gmail.com>
2019-06-20 14:09:18 -0700

    Accounts functionality

--- coHabs_project2/imports/ui/App.jsx
+++ coHabs_project2/imports/ui/App.jsx
@@ -5,15 +5,17 @@ import Item from './Item.jsx';
 import AddItem from './AddItem.jsx';
 import HeaderBar from './HeaderBar.jsx';
 import Welcome from './Welcome.jsx';
+import AccountsUIWrapper from './AccountsUIWrapper.jsx';
+
 
 class App extends Component {
   state = {
-    list : [
-      { name: 'Jess', type: 'Guests', message:'Gonna have 4-5 friends over tmrw', id : 1 },
-      { name:'Jason', type: 'Piano Practice', message:'practicing for recital tonight. Sorry about the noise!', id: 2 },
-      { name: 'Angelli', type: 'Guests', message:'gonna have 2 friends over on Thursday', id: 3 }
+    list: [
+      { name: 'Jess', type: 'Guests', message: 'Gonna have 4-5 friends over tmrw', id: 1 },
+      { name: 'Jason', type: 'Piano Practice', message: 'practicing for recital tonight. Sorry about the noise!', id: 2 },
+      { name: 'Angelli', type: 'Guests', message: 'gonna have 2 friends over on Thursday', id: 3 }
     ]
-  } 
+  }
   addItem = (item) => {
     item.id = Math.random();
     let list = [...this.state.list, item];
@@ -38,13 +40,19 @@ class App extends Component {
   }
   render() {
     return (
-    <div className="App">
-      <HeaderBar />
-      <Welcome />
-      <Item  deleteItem = { this.deleteItem} list= { this.state.list } />
-      <AddItem addItem= { this.addItem } />
-    </div>
-     );
+      <div className="App">
+        <HeaderBar />
+        <Welcome />
+        <Item deleteItem={this.deleteItem} list={this.state.list} />
+        <AddItem addItem={this.addItem} />
+
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <div>
+            <AccountsUIWrapper align="center" /></div>
+        </div>
+
+      </div>
+    );
   }
 }
 
