95ac981de5fdfe7c86640479f6910168ce8f20e2
Jason <jsykim33@gmail.com>
2019-08-07 17:29:45 -0700

    can grab amount from db

--- imports/ui/components/Pie Chart.js
+++ imports/ui/components/Pie Chart.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
 import CanvasJSReact from './canvasjs.react';
+import { Meteor } from 'meteor/meteor';
+import { Bills, Notes } from '../../api/notes';
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 
-class PieChart extends Component {
+class PieChart extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
 	render() {
+
+		let chartTitle = 'loading';
+
+		if (this.props.type !== undefined) {
+			chartTitle = this.props.type;
+		}
+
+		let bills = this.props.bills;
+
+		let amount = 3;
+		if (bills.length !== 0) {
+			// console.log("this.props.bills: " + JSON.stringify(this.props.bills));
+			// console.log("this.props.type: " + this.props.type);
+			let currentBill = this.props.bills.filter(bill => {
+				// console.log("bill type is: " + bill.type);
+				return bill.type === this.props.type
+			});
+
+			// console.log('current bill: ' + JSON.stringify(currentBill));
+
+			if (currentBill[0] !== undefined) {
+				amount = currentBill[0].amount;
+			}
+		}
+
+		// console.log('should say rent:' + this.props.type);
+		// console.log('bills props: ' + this.props.bills.rent);
+		// console.log(Bills.find({}).fetch());
+
+		// let groupMembersArray = [];
+
+		// if (this.props.groups.members !== undefined) {
+		// 	groupMembersArray = this.props.groups.members;
+		// 	console.log('group members array: ' + groupMembersArray);
+
+		// }
+
 		const options = {
 			exportEnabled: true,
 			animationEnabled: true,
 			title: {
-				text: "Rent"
+				text: chartTitle
 			},
 			data: [{
 				type: "pie",
@@ -20,9 +64,9 @@ class PieChart extends Component {
 				indexLabelFontSize: 16,
 				indexLabel: "{label} - {y}",
 				dataPoints: [
-					{ y: this.props.bills.amount / 3, label: "Angelli" },
-					{ y: 500 / 3, label: "Jason" },
-					{ y: 500, label: "Jess" },
+					{ y: Math.ceil(amount / 3), label: "Angelli" },
+					{ y: Math.ceil(amount / 3), label: "Jason" },
+					{ y: Math.ceil(amount / 3), label: "Jess" },
 
 				]
 			}]
@@ -37,6 +81,8 @@ class PieChart extends Component {
 				{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
 			</div>
 		);
+
+
 	}
 }
 
