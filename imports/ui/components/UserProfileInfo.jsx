feca0523b5cd898e1a37e37a7a8b3ed5c5ec2e40
Angelli Dimatulac <angellidimatulac@gmail.com>
2019-08-09 16:22:28 -0700

    stretch

--- imports/ui/components/UserProfileInfo.jsx
+++ imports/ui/components/UserProfileInfo.jsx
@@ -6,7 +6,7 @@ export default class UserProfileInfo extends React.Component {
     render() {
         return (
             <Container style={{ paddingTop: '70px', paddingLeft: '20px', paddingRight: '20px', paddingBottom: '20px' }}>
-                <Card>
+                <Card fluid>
                     <Card.Content>
                         <Card.Header>
                             { Meteor.user().profile.fname + ' ' + Meteor.user().profile.lname }
@@ -28,24 +28,6 @@ export default class UserProfileInfo extends React.Component {
                         </Card.Description>
                     </Card.Content>
                 </Card>
-                {/* <Header as='h2'>
-                    { Meteor.user().profile.fname + ' ' + Meteor.user().profile.lname }
-                    <Header.Subheader>
-                        @{ Meteor.user().username }
-                    </Header.Subheader>
-                </Header>
-                <Header textAlign='left'>
-                    <Header.Subheader>
-                        <Icon name='phone' size='large' color='grey' />
-                        { Meteor.user().profile.phone !== undefined ? Meteor.user().profile.phone : 'Enter a phone number' }
-                    </Header.Subheader>
-                </Header>
-                <Header textAlign='left'>
-                    <Header.Subheader>
-                        <Icon name='mail' size='large' color='grey' />
-                        { Meteor.user().profile.email !== undefined ? Meteor.user().profile.email : 'Enter an email address' }
-                    </Header.Subheader>
-                </Header> */}
             </Container>
         );
     }
